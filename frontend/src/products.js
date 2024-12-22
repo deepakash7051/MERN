669413d34bebe4dda0aed82086f889f87e1b0e72
Your Name <deepakash7051@gmail.com>
2024-12-22 23:55:31 +0530

    new changes

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -51,6 +51,58 @@ const products = [
     rating: 4.5,
     numReviews: 12,
   },
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphone",
+    image: "/images/sample.jpg",
+    description:
+      "LoeLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem at est ab, quod sapiente odit inventore soluta! Velit labore reiciendis voluptates qui optio animi veritatis repellendus, eius facere fuga voluptatibus.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 88.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Airpods Wireless Bluetooth Headphone",
+    image: "/images/phone.jpg",
+    description:
+      "LoeLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem at est ab, quod sapiente odit inventore soluta! Velit labore reiciendis voluptates qui optio animi veritatis repellendus, eius facere fuga voluptatibus.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 88.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "3",
+    name: "Airpods Wireless Bluetooth Headphone",
+    image: "/images/playstation.jpg",
+    description:
+      "LoeLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem at est ab, quod sapiente odit inventore soluta! Velit labore reiciendis voluptates qui optio animi veritatis repellendus, eius facere fuga voluptatibus.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 88.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Airpods Wireless Bluetooth Headphone",
+    image: "/images/sample.jpg",
+    description:
+      "LoeLorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem at est ab, quod sapiente odit inventore soluta! Velit labore reiciendis voluptates qui optio animi veritatis repellendus, eius facere fuga voluptatibus.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 88.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
 ];
 
 export default products;
